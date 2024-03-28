5d1f5e5a3b411cd1293e347a9c61b11827dd1c37
arizki07 <116191090+arizki07@users.noreply.github.com>
2024-03-28 23:18:15 +0700

    first commit

---
+++ resources/views/layout/main.blade.php
@@ -0,0 +1,43 @@
+@include('layout.partials.head')
+
+<!-- Begin page -->
+<div id="layout-wrapper">
+
+    @include('layout.partials.header')
+    <!-- ========== App Menu ========== -->
+    <div class="app-menu navbar-menu">
+        <!-- LOGO -->
+        @include('layout.partials.navbar')
+
+        @include('layout.partials.sidebar')
+
+        <div class="sidebar-background"></div>
+    </div>
+    <!-- Left Sidebar End -->
+    <!-- Vertical Overlay-->
+    <div class="vertical-overlay"></div>
+
+    <!-- ============================================================== -->
+    <!-- Start right Content here -->
+    <!-- ============================================================== -->
+    <div class="main-content">
+
+        @yield('content')
+        <!-- End Page-content -->
+
+        @include('layout.partials.footer')
+    </div>
+    <!-- end main content-->
+
+</div>
+<!-- END layout-wrapper -->
+
+
+
+@include('layout.partials.themes_setting')
+
+@include('layout.partials.script')
+
+</body>
+
+</html>
