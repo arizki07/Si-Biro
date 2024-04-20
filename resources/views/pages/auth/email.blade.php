f737a1c5e56595d45af4d95c9766051f181a8041
Alvi <mohalviridho@gmail.com>
2024-04-20 19:57:53 +0700

    feat: Register

---
+++ resources/views/pages/auth/email.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Verifikasi Kode OTP</title>
+</head>
+
+<body>
+    <p>Terima kasih telah bergabung dengan aplikasi Biro kami.</p>
+    <p>Berikut adalah kode OTP Anda: <strong>{{ $otp }}</strong></p>
+    <p>Mohon gunakan kode tersebut untuk verifikasi akun Anda.</p>
+    <p>Terima kasih.</p>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Admin\BiodataController;
 use App\Http\Controllers\Admin\DataUserController;
 use App\Http\Controllers\Admin\ImportController;
 use App\Http\Controllers\Admin\JadwalController;
+use App\Http\Controllers\Admin\KeuanganController;
 use App\Http\Controllers\Admin\LayananController;
 use App\Http\Controllers\Admin\RoleController;
 use App\Http\Controllers\Admin\TransaksiController;
@@ -21,40 +22,13 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::get('register', [AuthController::class, 'register'])->name('register');
 Route::post('register', [AuthController::class, 'registerPost']);
 Route::get('/otp-verification', [AuthController::class, 'otpVerification'])->name('otpVerification');
+Route::get('/generate-new-otp', [AuthController::class, 'generateNewOTP'])->name('generateNewOTP');
 Route::post('/otp-verification', [AuthController::class, 'otpVerificationPost']);
 
 // <<<<<<< login
 Route::middleware('auth')->group(function () {
     Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-// =======
-// Route::controller(DashboardController::class)->group(function () {
-//     Route::get('/dashboard', 'index');
-// });
-// Route::controller(App\Http\Controllers\Admin\DataUserController::class)->group(function () {
-//     Route::get('/data-users', 'index');
-//     Route::post('/user/create', 'add')->name('user.create');
-// });
-// Route::controller(App\Http\Controllers\Admin\BiodataController::class)->group(function () {
-//     Route::get('/data-biodata', 'index')->name('index.biodata');
-//     Route::post('/store-biodata', 'store')->name('store.biodata');
-// });
-// Route::controller(App\Http\Controllers\Admin\TransaksiController::class)->group(function () {
-//     Route::get('/data-transaksi', 'index');
-// });
-// Route::controller(App\Http\Controllers\Admin\RoleController::class)->group(function () {
-//     Route::get('/data-role', 'index')->name('index.role');
-//     Route::post('/store/role', 'store')->name('store.role');
-//     Route::post('/update/role/{id}', 'update')->name('update.role');
-//     Route::delete('/destroy/role/{id}', 'destroy')->name('destroy.role');
-// });
-// Route::controller(App\Http\Controllers\Admin\JadwalController::class)->group(function () {
-//     Route::get('/data-jadwal', 'index');
-// });
-// Route::controller(App\Http\Controllers\Admin\LayananController::class)->group(function () {
-//     Route::get('/data-layanan', 'index');
-// });
-// >>>>>>> main
 
     Route::get('/data-users', [DataUserController::class, 'index'])->name('data.users');
     Route::get('/data-biodata', [BiodataController::class, 'index'])->name('data.biodata');
@@ -67,4 +41,5 @@ Route::middleware('auth')->group(function () {
     Route::get('/data-jadwal', [JadwalController::class, 'index'])->name('data.jadwal');
     Route::get('/data-layanan', [LayananController::class, 'index'])->name('data.layanan');
     Route::post('/import', [ImportController::class, 'import'])->name('import');
+    Route::get('/data-keuangan', [KeuanganController::class, 'index'])->name('data.keuangan');
 });
